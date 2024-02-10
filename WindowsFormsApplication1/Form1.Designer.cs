ac232a3f763db784a5e9012c9b8c8aee99aff155
LevkulichOV <LevkulichOV@C7A106-N8>
2024-02-10 12:13:00 +0300

    Добавьте файлы проекта.

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,309 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(105, 9);
+            this.label1.Name = "label1";
+            this.label1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label1.Size = new System.Drawing.Size(137, 31);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Студенты";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button1.Location = new System.Drawing.Point(31, 71);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(97, 43);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Гулина";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button2.Location = new System.Drawing.Point(227, 71);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(97, 43);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Дегтярев";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(12, 145);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(349, 234);
+            this.tabControl1.TabIndex = 4;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.button6);
+            this.tabPage1.Controls.Add(this.button5);
+            this.tabPage1.Controls.Add(this.button4);
+            this.tabPage1.Controls.Add(this.textBox2);
+            this.tabPage1.Controls.Add(this.label3);
+            this.tabPage1.Controls.Add(this.textBox1);
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(341, 208);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Гулина";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            this.tabPage1.Click += new System.EventHandler(this.tabPage1_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(244, 120);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(64, 59);
+            this.button6.TabIndex = 13;
+            this.button6.Text = "Показать все оценки";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(89, 105);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(93, 20);
+            this.button5.TabIndex = 12;
+            this.button5.Text = "Убрать";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(89, 41);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(93, 20);
+            this.button4.TabIndex = 11;
+            this.button4.Text = "Добавить";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(15, 106);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(48, 20);
+            this.textBox2.TabIndex = 10;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 78);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(170, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Какую оценку вы хотите убрать:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(15, 41);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(48, 20);
+            this.textBox1.TabIndex = 8;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 15);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(183, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Какую оценку вы хотите добавить:";
+            this.label2.Click += new System.EventHandler(this.label2_Click_1);
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.button7);
+            this.tabPage2.Controls.Add(this.button8);
+            this.tabPage2.Controls.Add(this.button9);
+            this.tabPage2.Controls.Add(this.textBox3);
+            this.tabPage2.Controls.Add(this.label4);
+            this.tabPage2.Controls.Add(this.textBox4);
+            this.tabPage2.Controls.Add(this.label5);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(341, 208);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Дегтярев";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            this.tabPage2.Click += new System.EventHandler(this.tabPage2_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(243, 120);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(65, 58);
+            this.button7.TabIndex = 13;
+            this.button7.Text = "Показать все оценки";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(89, 101);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(93, 20);
+            this.button8.TabIndex = 12;
+            this.button8.Text = "Убрать";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(89, 40);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(93, 20);
+            this.button9.TabIndex = 11;
+            this.button9.Text = "Добавить";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(14, 101);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(48, 20);
+            this.textBox3.TabIndex = 10;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 73);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(170, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Какую оценку вы хотите убрать:";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(15, 40);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(48, 20);
+            this.textBox4.TabIndex = 8;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(11, 14);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(183, 13);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Какую оценку вы хотите добавить:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.ClientSize = new System.Drawing.Size(373, 391);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,233 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        Student Program = new Student();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            tabControl1.TabPages[0].Text = "Гулина";
+            tabControl1.SelectedTab = tabPage1;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            tabControl1.TabPages[1].Text = "Дегтярев";
+            tabControl1.SelectedTab = tabPage2;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (int.TryParse(textBox1.Text, out int result))
+            {
+                Program.Plus(result);
+                textBox1.Clear();
+            }
+            else
+            {
+                MessageBox.Show("Должно быть целое число");
+                textBox1.Clear();
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (int.TryParse(textBox2.Text, out int result))
+            {
+                Program.Minus(result);
+                textBox2.Clear();
+            }
+            else
+            {
+                MessageBox.Show("Должно быть целое число");
+                textBox2.Clear();
+            }
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            if (int.TryParse(textBox4.Text, out int result))
+            {
+                Program.Plus1(result);
+                textBox4.Clear();
+            }
+            else
+            {
+                MessageBox.Show("Должно быть целое число");
+                textBox4.Clear();
+            }
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            if (int.TryParse(textBox3.Text, out int result))
+            {
+                Program.Minus1(result);
+                textBox3.Clear();
+            }
+            else
+            {
+                MessageBox.Show("Должно быть целое число");
+                textBox3.Clear();
+            }
+        }
+
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            Program.DisplayAccountInfo();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            Program.DisplayAccountInfo1();
+        }
+
+        private void tabPage1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tabPage2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+class Student // Объявление класса Student
+{
+    // Приватные переменные для хранения информации 
+    private double three;
+    private double four;
+    private double five;
+    double max = 10;
+
+    private double three1;
+    private double four1;
+    private double five1;
+
+    public void Plus(double score) //Добавление оценок
+    {
+        if (score == 5 && five < max)
+        {
+            five++;
+            MessageBox.Show($"Добавлена оценка 5. Текущее количество: {five} шт.");
+        }
+        else if (score == 4 && four < max)
+        {
+            four++;
+            MessageBox.Show($"Добавлена оценка 4. Текущее количество: {four} шт.");
+        }
+        else if (score == 3 && three < max)
+        {
+            three++;
+            MessageBox.Show($"Добавлена оценка 3. Текущее количество: {three} шт.");
+        }
+        else
+        {
+            MessageBox.Show("Некорректная оценка или превышено максимальное количество оценок.");
+        }
+    }
+    public void Plus1(double score) //Добавление оценок
+    {
+        if (score == 5 && five1 < max)
+        {
+            five1++;
+            MessageBox.Show($"Добавлена оценка 5. Текущее количество: {five1} шт.");
+        }
+        else if (score == 4 && four1 < max)
+        {
+            four1++;
+            MessageBox.Show($"Добавлена оценка 4. Текущее количество: {four1} шт.");
+        }
+        else if (score == 3 && three1 < max)
+        {
+            three1++;
+            MessageBox.Show($"Добавлена оценка 3. Текущее количество: {three1} шт.");
+        }
+        else
+        {
+            MessageBox.Show("Некорректная оценка или превышено максимальное количество оценок.");
+        }
+    }
+    public void Minus(double score) //Снятие ошибок
+    {
+        if (score == 5 && five > 0)
+        {
+            five--;
+            MessageBox.Show($"Снята оценка 5. Текущее количество: {five} шт.");
+        }
+        else if (score == 4 && four > 0)
+        {
+            four--;
+            MessageBox.Show($"Снята оценка 4. Текущее количество: {four} шт.");
+        }
+        else if (score == 3 && three > 0)
+        {
+            three--;
+            MessageBox.Show($"Снята оценка 3. Текущее количество: {three} шт.");
+        }
+        else
+        {
+            MessageBox.Show("Некорректная оценка или отсутствуют оценки для снятия.");
+        }
+    }
+    public void Minus1(double score) //Снятие ошибок
+    {
+        if (score == 5 && five1 > 0)
+        {
+            five1--;
+            MessageBox.Show($"Снята оценка 5. Текущее количество: {five1} шт.");
+        }
+        else if (score == 4 && four1 > 0)
+        {
+            four1--;
+            MessageBox.Show($"Снята оценка 4. Текущее количество: {four1} шт.");
+        }
+        else if (score == 3 && three1 > 0)
+        {
+            three1--;
+            MessageBox.Show($"Снята оценка 3. Текущее количество: {three1} шт.");
+        }
+        else
+        {
+            MessageBox.Show("Некорректная оценка или отсутствуют оценки для снятия.");
+        }
+    }
+
+    public void DisplayAccountInfo() // Метод для вывода информации об аккаунте
+    {
+        MessageBox.Show($"Оценки: \nПятерки: {five} шт. \nЧетверки: {four} шт. \nТройки: {three} шт.");
+    }
+    public void DisplayAccountInfo1() // Метод для вывода информации об аккаунте
+    {
+        MessageBox.Show($"Оценки: \nПятерки: {five1} шт. \nЧетверки: {four1} шт. \nТройки: {three1} шт.");
+    }
+}
